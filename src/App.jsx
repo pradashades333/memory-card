648a65d8b0baff104987004a610cc7d783a23e16
pradashades333 <alexdreve03@gmail.com>
2026-02-22 23:44:00 +0100

    changed up the files again and added pokemon api thing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { useState, useEffect } from 'react';
 import GameBoard from './components/GameBoard';
 import ScoreBoard from './components/ScoreBoard';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=12')
+      .then(res => res.json())
+      .then(data => {
+        const fetches = data.results.map(p =>
+          fetch(p.url).then(res => res.json())
+        );
+        return Promise.all(fetches);
+      })
+      .then(pokemonData => {
+        setPokemon(pokemonData);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Memory Card Game</h1>
       <ScoreBoard />
-      <GameBoard />
+      <GameBoard pokemon={pokemon} />
     </div>
   );
 }
---
+++ src/components/Score.jsx
@@ -0,0 +1,9 @@
+function Score() {
+  return (
+    <div>
+      <p>ScoreBoard</p>
+    </div>
+  );
+}
+
+export default Score;
